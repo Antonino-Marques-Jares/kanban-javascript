633b89e8661841e0bad648d010e31f5289c6cf5e
Antonino Marques Jares <144476542+Antonino-Marques-Jares@users.noreply.github.com>
2024-05-06 12:23:36 -0500

    Kanban
    Tratando de eventos arrastar e soltar e elementos dinâmicos

---
+++ script.js
@@ -0,0 +1,36 @@
+const grupos = document.querySelectorAll(".grupo");
+
+document.addEventListener("dragstart", (e) => {
+  e.target.classList.add("dragging");
+});
+
+document.addEventListener("dragend", (e) => {
+  e.target.classList.remove("dragging");
+});
+
+grupos.forEach((item) => {
+  item.addEventListener("dragover", (e) => {
+    const dragging = document.querySelector(".dragging");
+    const applyAfter = getNewPosition(item, e.clientY);
+
+    if (applyAfter) {
+      applyAfter.insertAdjacentElement("afterend", dragging);
+    } else {
+      item.prepend(dragging);
+    }
+  });
+});
+
+function getNewPosition(grupo, posY) {
+  const cards = grupo.querySelectorAll(".item:not(.dragging)");
+  let result;
+
+  for (let refer_card of cards) {
+    const box = refer_card.getBoundingClientRect();
+    const boxCenterY = box.y + box.height / 2;
+
+    if (posY >= boxCenterY) result = refer_card;
+  }
+
+  return result;
+}
